c6882ef7d78e5744a0a574b21129b24c0852e655
Tuetuopay <tuetuopay@me.com>
2021-01-25 13:06:56 +0000

    Add examples

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,30 @@ use tracing::{
  * error (resource not found, permission denied, ...).
  *
  * Basically, a server that yields a Next error should be considered unhealthy.
+ *
+ * # Example
+ *
+ * ```rust
+ * # use tourniquet::Next;
+ * #
+ * enum MyError {
+ *     NotFound,
+ *     PermissionDenied,
+ *     InternalError,
+ *     Timeout,
+ * }
+ *
+ * impl Next for MyError {
+ *     fn is_next(&self) -> bool {
+ *         match self {
+ *             // Business logic error, that are likely to happen on all servers
+ *             Self::NotFound | Self::PermissionDenied => false,
+ *             // Server specific error: server software down, host down, etc. Try the next one
+ *             Self::InternalError | Self::Timeout => true,
+ *         }
+ *     }
+ * }
+ * ```
  */
 pub trait Next {
     /**
@@ -39,9 +63,45 @@ pub trait Next {
     fn is_next(&self) -> bool;
 }
 
+impl Next for std::io::Error {
+    fn is_next(&self) -> bool {
+        use std::io::ErrorKind::*;
+        match self.kind() {
+            ConnectionRefused | ConnectionReset | ConnectionAborted | NotConnected | BrokenPipe
+            | TimedOut | Interrupted | UnexpectedEof => true,
+            NotFound | PermissionDenied | AddrInUse | AddrNotAvailable | AlreadyExists
+            | WouldBlock | InvalidInput | InvalidData | Other => false,
+            _ => false,
+        }
+    }
+}
+
 /**
  * Trait to be implemented by connector types. Used to get a connected service from its connection
  * information.
+ *
+ * Note that the implementor type can hold data for connection information shared across all
+ * providers, like TLS certificates, port, database name, ...
+ *
+ * # Example
+ *
+ * ```rust
+ * # use async_trait::async_trait;
+ * # use std::sync::Mutex;
+ * use std::{io::Error, net::IpAddr};
+ * use tokio::net::TcpStream;
+ * use tourniquet::Connector;
+ *
+ * struct Conn(u16);
+ *
+ * #[async_trait]
+ * impl Connector<IpAddr, Mutex<TcpStream>, Error> for Conn {
+ *     async fn connect(&self, src: &IpAddr) -> Result<Mutex<TcpStream>, Error> {
+ *         let Conn(ref port) = self;
+ *         TcpStream::connect((*src, *port)).await.map(Mutex::new)
+ *     }
+ * }
+ * ```
  */
 #[async_trait]
 pub trait Connector<SvcSrc, Svc, E> {
@@ -53,6 +113,41 @@ pub trait Connector<SvcSrc, Svc, E> {
  *
  * This holds a list of services, a way to connect to said services, and a way to run stuff against
  * this connected service.
+ *
+ * # Example
+ *
+ * ```rust,no_run
+ * # use async_trait::async_trait;
+ * use std::{io::Error, net::IpAddr};
+ * use tokio::{io::AsyncReadExt, net::TcpStream, sync::Mutex};
+ * use tourniquet::{Connector, RoundRobin};
+ *
+ * struct Conn(u16);
+ *
+ * #[async_trait]
+ * impl Connector<IpAddr, Mutex<TcpStream>, Error> for Conn {
+ *     async fn connect(&self, src: &IpAddr) -> Result<Mutex<TcpStream>, Error> {
+ *         let Conn(ref port) = self;
+ *         TcpStream::connect((*src, *port)).await.map(Mutex::new)
+ *     }
+ * }
+ *
+ * # #[tokio::main]
+ * # async fn main() {
+ * let rr = RoundRobin::new(
+ *     vec!["185.30.166.38".parse().unwrap(), "66.110.9.37".parse().unwrap()],
+ *     Conn(6667),
+ * );
+ *
+ * let hello = rr.run(|sock| async move {
+ *     let mut sock = sock.lock().await;
+ *     let mut buf = [0; 50];
+ *     sock.read_exact(&mut buf).await.map(|_| String::from_utf8(buf.to_vec()).unwrap())
+ * }).await.unwrap();
+ *
+ * assert!(hello.contains("freenode.net"));
+ * # }
+ * ```
  */
 pub struct RoundRobin<SvcSrc, Svc, E, Conn>
 where
@@ -87,10 +182,37 @@ where
     /**
      * Build a new round-robin manager.
      *
-     * The connector is an async function (or closure returning a future) that yields a connected
-     * handler to the service, from its service "source" (usually an URL).
+     * The connector is a struct that yields a connected handler to the service, from its service
+     * "source" (usually an URL). Note that the connector is lazily executed on demand when a
+     * connection is needed.
      *
-     * Note that the connector function is not run at this point.
+     * # Example
+     *
+     * ```rust
+     * # use async_trait::async_trait;
+     * # use std::sync::Mutex;
+     * use std::{io::Error, net::IpAddr};
+     * use tokio::net::TcpStream;
+     * use tourniquet::{Connector, RoundRobin};
+     *
+     * struct Conn(u16);
+     *
+     * #[async_trait]
+     * impl Connector<IpAddr, Mutex<TcpStream>, Error> for Conn {
+     *     async fn connect(&self, src: &IpAddr) -> Result<Mutex<TcpStream>, Error> {
+     *         let Conn(ref port) = self;
+     *         TcpStream::connect((*src, *port)).await.map(Mutex::new)
+     *     }
+     * }
+     *
+     * # #[tokio::main]
+     * # async fn main() {
+     * let rr = RoundRobin::new(
+     *     vec!["185.30.166.38".parse().unwrap(), "66.110.9.37".parse().unwrap()],
+     *     Conn(6667),
+     * );
+     * # }
+     * ```
      */
     pub fn new(sources: Vec<SvcSrc>, connector: Conn) -> Self {
         Self {
